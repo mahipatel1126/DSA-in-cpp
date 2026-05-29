a47339c513d98eb020df25dab6768e14cb66433c
mahipatel1126 <patelmm1126@gmail.com>
2026-05-29 19:47:50 +0530

    Added subsequence solution

---
+++ String/subsequence.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+};
+
+int main() {
+    Solution obj;
+
+    string s, t;
+
+    cout << "Enter string s: ";
+    cin >> s;
+
+    cout << "Enter string t: ";
+    cin >> t;
+
+    if (obj.isSubsequence(s, t)) {
+        cout << "true" << endl;
+    } else {
+        cout << "false" << endl;
+    }
+
+    return 0;
+}
